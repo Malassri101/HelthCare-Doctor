7f7752251ad1711815f6847ed662179d72c8bda7
Malassri101 <malassri97@gmail.com>
2020-05-05 23:12:11 +0530

    All the revevant files Added

--- src/main/java/com/HelthCare/Model/Doctor.java
+++ src/main/java/com/HelthCare/Model/Doctor.java
@@ -1,71 +1,155 @@
 package com.HelthCare.Model;
 
-public class Doctor {
 
-	public String docID;
-	public String docFname;
-	public String docSname;
-	public String docEmail;
-	public String docMobile;
-	public String docSpecialization;
-	public String docIntime;
-	public String docOutime;
-	
-	@Override
-	public String toString() {
-		return "Doctor [docID=" + docID + ", docFname=" + docFname + ", docSname=" + docSname + ", docEmail=" + docEmail
-				+ ", docMobile=" + docMobile + ", docSpecialization=" + docSpecialization + ", docIntime=" + docIntime
-				+ ", docOutime=" + docOutime + "]";
-	}
-	
-	public String getDocIntime() {
-		return docIntime;
-	}
-	public void setDocIntime(String docIntime) {
-		this.docIntime = docIntime;
-	}
-	public String getDocOutime() {
-		return docOutime;
-	}
-	public void setDocOutime(String docOutime) {
-		this.docOutime = docOutime;
-	}
-	public String getDocID() {
-		return docID;
-	}
-	public void setDocID(String docID) {
-		this.docID = docID;
-	}
-	public String getDocFname() {
-		return docFname;
-	}
-	public void setDocFname(String docFname) {
-		this.docFname = docFname;
-	}
-	public String getDocSname() {
-		return docSname;
-	}
-	public void setDocSname(String docSname) {
-		this.docSname = docSname;
-	}
-	public String getDocEmail() {
-		return docEmail;
-	}
-	public void setDocEmail(String docEmail) {
-		this.docEmail = docEmail;
-	}
-	public String getDocMobile() {
-		return docMobile;
-	}
-	public void setDocMobile(String docMobile) {
-		this.docMobile = docMobile;
-	}
-	public String getDocSpecialization() {
-		return docSpecialization;
-	}
-	public void setDocSpecialization(String docSpecialization) {
-		this.docSpecialization = docSpecialization;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class Item {
+	private Connection connect() {
+		Connection con = null;
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/test_paf", "root", "");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return con;
 	}
 
-	
+	public String readItems() {
+		String output = "";
+
+		try {
+			Connection con = connect();
+			if (con == null) {
+
+				return "Error while connecting to the database for reading.";
+			}
+
+			// Prepare the html table to be displayed
+
+			output = "<table border='1'><tr><th>Item Code</th>" + "<th>Item Name</th><th>Item Price</th>"
+					+ "<th>Item Description</th>" + "<th>Update</th><th>Remove</th></tr>";
+
+			String query = "select * from items";
+			Statement stmt = con.createStatement();
+			ResultSet rs = stmt.executeQuery(query);
+			// iterate through the rows in the result set
+			while (rs.next()) {
+				String itemID = Integer.toString(rs.getInt("itemID"));
+				String itemCode = rs.getString("itemCode");
+				String itemName = rs.getString("itemName");
+				String itemPrice = Double.toString(rs.getDouble("itemPrice"));
+				String itemDesc = rs.getString("itemDesc");
+				
+				// Add into the html table
+				output += "<tr><td><input id='hidItemIDUpdate'" + "name='hidItemIDUpdate'" + "type='hidden' value='"
+						+ itemID + "'>" + itemCode + "</td>";
+				
+				output += "<td>" + itemName + "</td>";
+				output += "<td>" + itemPrice + "</td>";
+				output += "<td>" + itemDesc + "</td>";
+				// buttons
+				output += "<td><input name='btnUpdate'" + "type='button' value='Update'"
+						+ "class='btnUpdate btn btn-secondary'></td>" + "<td><input name='btnRemove'"
+						+ "type='button' value='Remove'" + "class='btnRemove btn btn-danger'" + "data-itemid='" + itemID
+						+ "'>" + "</td></tr>";
+			}
+			con.close();
+			// Complete the html table
+			output += "</table>";
+		} catch (Exception e) {
+			output = "Error while reading the items.";
+			System.err.println(e.getMessage());
+		}
+		return output;
+	}
+
+	public String insertItem(String code, String name, String price, String desc) {
+		String output = "";
+		try {
+			Connection con = connect();
+			if (con == null) {
+				return "Error while connecting to the database for inserting.";
+			}
+			// create a prepared statement
+			String query = " insert into items(`itemID`,`itemCode`,`itemName`,`itemPrice`,`itemDesc`)"
+					+ " values (?, ?, ?, ?, ?)";
+			PreparedStatement preparedStmt = con.prepareStatement(query);
+			// binding values
+			preparedStmt.setInt(1, 0);
+			preparedStmt.setString(2, code);
+			preparedStmt.setString(3, name);
+			preparedStmt.setDouble(4, Double.parseDouble(price));
+			preparedStmt.setString(5, desc);
+
+			// execute the statement
+			preparedStmt.execute();
+			con.close();
+
+			String newItems = readItems();
+			output = "{\"status\":\"success\", \"data\": \"" + newItems + "\"}";
+		} catch (Exception e) {
+			output = "{\"status\":\"error\", \"data\":" + " \"Error while inserting the item.\"}";
+			System.err.println(e.getMessage());
+		}
+		return output;
+	}
+
+	public String updateItem(String ID, String code, String name, String price, String desc) {
+		String output = "";
+		try {
+			Connection con = connect();
+			if (con == null) {
+				return "Error while connecting to the database for updating.";
+			}
+			// create a prepared statement
+			String query = "UPDATE items SET itemCode=?,itemName=?,itemPrice=?,itemDesc=? WHERE itemID=?";
+			PreparedStatement preparedStmt = con.prepareStatement(query);
+			// binding values
+			preparedStmt.setString(1, code);
+			preparedStmt.setString(2, name);
+			preparedStmt.setDouble(3, Double.parseDouble(price));
+			preparedStmt.setString(4, desc);
+			preparedStmt.setInt(5, Integer.parseInt(ID));
+			// execute the statement
+			preparedStmt.execute();
+			con.close();
+			String newItems = readItems();
+			output = "{\"status\":\"success\", \"data\": \"" + newItems + "\"}";
+		} catch (Exception e) {
+			output = "{\"status\":\"error\", \"data\":" + "\"Error while updating the item.\"}";
+			System.err.println(e.getMessage());
+		}
+		return output;
+	}
+
+	public String deleteItem(String itemID) {
+		String output = "";
+		try {
+			Connection con = connect();
+			if (con == null) {
+				return "Error while connecting to the database for deleting.";
+			}
+
+			// create a prepared statement
+			String query = "delete from items where itemID=?";
+			PreparedStatement preparedStmt = con.prepareStatement(query);
+			// binding values
+			preparedStmt.setInt(1, Integer.parseInt(itemID));
+			// execute the statement
+			preparedStmt.execute();
+			con.close();
+			String newItems = readItems();
+			output = "{\"status\":\"success\", \"data\": \"" + newItems + "\"}";
+		} catch (Exception e) {
+			output = "{\"status\":\"error\", \"data\":" + "\"Error while deleting the item.\"}";
+			System.err.println(e.getMessage());
+		}
+		return output;
+	}
+
 }
